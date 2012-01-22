e5e5731ec78dbaad34cffd62d4387ae0861f97e0
Freek Wielstra <fr.wielstra@gmail.com>
2012-01-22 20:32:09 +0100

    Added a few more rooms so we can go all directions

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -27,7 +27,7 @@ case class Room(title: String, description: String, exitMap: Stream[Map[Directio
        | {description}
        |
        | Exits:
-       |  {exits}""".stripMargin
+       |{exits}""".stripMargin
 
   override def toString() = {
     Formatter.parse(outputFormat, Map(
@@ -39,15 +39,18 @@ case class Room(title: String, description: String, exitMap: Stream[Map[Directio
 
 // lists all the rooms in this 'level'.
 object Rooms {
-  def _exitMap(mappedItems: => Map[Direction.Direction, Room]) = {
+  def exits(mappedItems: => Map[Direction.Direction, Room]) = {
     Map[Direction.Direction, Room]() #::
     mappedItems #::
     Stream.empty
   }
 
   import Direction._
-  lazy val first: Room = Room("North room", "This is the north room. It is very big. There is shit on the wall.", _exitMap(Map(South -> second)))
-  lazy val second: Room = Room("South room", "This is the south room. It is small. There are small people here that want to eat you. Om nom nom nom.", _exitMap(Map(North -> first)))
+  lazy val northRoom:Room = Room("North room", "This is the north room. It is very big. There is shit on the wall.", exits(Map(South -> mainRoom)))
+  lazy val eastRoom: Room = Room("East Room", "This is the east room. It is full of puppies", exits(Map(West -> mainRoom)))
+  lazy val southRoom:Room = Room("South room", "This is the south room. It is small. There are small people here that want to eat you. Om nom nom nom.", exits(Map(North -> mainRoom)))
+  lazy val westRoom: Room = Room("West room", "This is the west room. Cocks cocks cocks cocks cocks cocks", exits(Map(East -> mainRoom)))
+  lazy val mainRoom: Room = Room("Main room", "This is the main room. There are rooms going in all direction from here.", exits(Map(North -> northRoom, East -> eastRoom, South -> southRoom, West -> westRoom)))
 }
             
 class Game(initialLocation: Room) {
@@ -67,9 +70,10 @@ object main {
 
     println("Welcome to Fuck Yeah Awesome Pre-Beta 3, the Game that will Give you a Huge Boner.")
     var isRunning = true
-    val game = new Game(Rooms.first)
+    val game = new Game(Rooms.mainRoom)
 
     while(isRunning) {
+      println()
       println(game.currentLocation)
       val input = readLine
       // parse the input
@@ -79,7 +83,7 @@ object main {
         val direction = Direction.parseInput(input)
         game.go(direction)
       }
-      println("bai")
     }
+    println("bai")
   }
 }
