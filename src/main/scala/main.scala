9b6234c91ebab4a0a4b27ad8206d5863fd736ee5
Freek Wielstra <fr.wielstra@gmail.com>
2012-01-22 20:06:17 +0100

    Redid how rooms are structured, now uses a Stream with lazy vars of rooms. Encountered that it's hard to refer to room A from within room B if you're still defining room A. With this approach, each room can be initialized separately, then initialized and linked up all in one go - with lazy vars, too, so they're only actually initialized when the user visits them.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,9 +6,21 @@ object Direction extends Enumeration {
   val East = Value("East")
   val South = Value("South")
   val West = Value("West")
+  val Unknown = Value("")
+  
+  def parseInput(input: String) = {
+    Direction.withName(input)
+  }
 }
 
-case class Room(title: String, exits: Seq[(Direction.Direction, Room)] = Nil) {
+// use streams / lazily initialized values to create a network of rooms.
+// rather fancy really.
+// http://stackoverflow.com/questions/8962044/how-do-i-refer-to-a-variable-while-assigning-a-value-to-it-whilst-retaining-imm 
+case class Room(title: String, exitMap: Stream[Map[Direction.Direction, Room]]) {
+  def exits = exitMap(1) // skip the Streams empty head
+  def hasExit(direction: Direction.Direction) = exits contains direction
+  def exit(inDirection: Direction.Direction) = exits(inDirection)
+
   val outputFormat =
     """|{BOLD}{title}{/BOLD}
        | Exits:
@@ -16,23 +28,54 @@ case class Room(title: String, exits: Seq[(Direction.Direction, Room)] = Nil) {
 
   override def toString() = {
     Formatter.parse(outputFormat, Map(
-      "title" -> title,
-      "exits" -> exits.map(exit => "  %s\n".format(exit._1)).mkString))
+    "title" -> title,
+    "exits" -> exits.map(exit => "  %s\n".format(exit._1)).mkString))
   }
 }
 
+// lists all the rooms in this 'level'.
+object Rooms {
+  def _exitMap(mappedItems: => Map[Direction.Direction, Room]) = {
+    Map[Direction.Direction, Room]() #::
+    mappedItems #::
+    Stream.empty
+  }
+
+  import Direction._
+  lazy val first: Room = Room("North room", _exitMap(Map(South -> second)))
+  lazy val second: Room = Room("South room", _exitMap(Map(North -> first)))
+}
+            
 class Game(initialLocation: Room) {
    var currentLocation = initialLocation
+
+   def go(direction: Direction.Direction) {
+    if (currentLocation.hasExit(direction)) {
+      currentLocation = currentLocation.exit(direction)
+    }
+   }
+
    override def toString = "current location: %s".format(currentLocation)
 }
 
 object main {
-  def print(text: Any) = println("%s%s".format(Style.ENDC, text))
   def main(args: Array[String]) {
     import Direction._
 
-    val roomOne = Room("A room", Seq((North, Room("Another room"))))
-    val game = new Game(roomOne)
-    print(roomOne)
+    var isRunning = true
+    val game = new Game(Rooms.first)
+
+    while(isRunning) {
+      println(game.currentLocation)
+      val input = readLine
+      // parse the input
+      if (input == "exit") {
+        isRunning = false
+      } else {
+        val direction = Direction.parseInput(input)
+        game.go(direction)
+      }
+      println("bai")
+    }
   }
 }
