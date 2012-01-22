5f808b57821afdaf8298fe874ee99927e2a4533c
Freek Wielstra <fr.wielstra@gmail.com>
2012-01-22 14:23:56 +0100

    cleanup, add output format method.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,17 +7,18 @@ object Direction extends Enumeration {
 
 object Style {
   private val CSI = "\033["
+  def addStyle(content: String, style: String = Style.NONE) = "%s %s".format(style, content)
+
+  val NONE = ""
   val BOLD = CSI + "1m"
 }
 
-
 case class Room(title: String, exits: Seq[(Direction.Direction, Room)] = Nil) {
-  override def toString() = "%s %s".format(Style.BOLD, title)
+  override def toString() = Style.addStyle(title, Style.BOLD)
 }
 
 object main {
   def main(args: Array[String]) {
-    println("ohai")
     import Direction._
 
     val roomOne = Room("A room", Seq((North, Room("Another room"))))
